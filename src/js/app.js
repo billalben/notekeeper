39e0a07d5f8ad6cfa6108fb649b4b61aaf22124e
Benzazoua <billalben252@gmail.com>
2024-01-29 21:33:58 +0100

    Basic JS Functionality

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,17 @@
 "use strict";
+
+// Module import
+import { addEventOnElements } from "./utils.js";
+
+const /** {HTMLElement} */ $sidebar = document.querySelector("[data-sidebar]");
+const /** {NodeList} */ $sidebarTogglers = document.querySelectorAll(
+    "[data-sidebar-toggler]"
+  );
+const /** {HTMLElement} */ $overlay = document.querySelector(
+    "[data-sidebar-overlay]"
+  );
+
+addEventOnElements($sidebarTogglers, "click", function () {
+  $sidebar.classList.toggle("active");
+  $overlay.classList.toggle("active");
+});
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,1 +1,16 @@
 "use strict";
+
+/**
+ * Attaches an event listener to a collection of DOM elements
+ * @param {NodeList} $elements - An array of DOM elements to attach the event listener to.
+ * @param {string} eventType - The type of event to listen for (e.g., 'click', 'mouseover')
+ * @param {Function} callback - The function to be executed when the event occur
+ */
+
+const addEventOnElements = function ($elements, eventType, callback) {
+  for (const item of $elements) {
+    item.addEventListener(eventType, callback);
+  }
+};
+
+export { addEventOnElements };

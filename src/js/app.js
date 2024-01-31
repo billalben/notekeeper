5b8694973d0a625dd714e51bd4637075fa025da3
Benzazoua <billalben252@gmail.com>
2024-01-31 21:30:45 +0100

    Tooltip Functionality

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@
 
 // Module import
 import { addEventOnElements, getGreetingMsg } from "./utils.js";
+import { Tooltip } from "./components/Tooltip.js";
 
 const /** {HTMLElement} */ $sidebar = document.querySelector("[data-sidebar]");
 const /** {NodeList} */ $sidebarTogglers = document.querySelectorAll(
@@ -15,6 +16,11 @@ addEventOnElements($sidebarTogglers, "click", function () {
   $sidebar.classList.toggle("active");
   $overlay.classList.toggle("active");
 });
+
+// Initialize tooltip behavior for all DOM elements with 'data-tooltip' attribute
+const /** {NodeList} */ $tooltipElements =
+    document.querySelectorAll("[data-tooltip]");
+$tooltipElements.forEach(($elem) => Tooltip($elem));
 
 // Show greeting message on homepage
 const /** {HTMLElement} */ $greetElement =
